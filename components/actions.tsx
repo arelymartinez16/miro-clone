3f9af3d4d7dfa73fd933fa055e2c0c46d7df5341
arelymartinez16 <arelymartinez741@gmail.com>
2024-07-26 23:58:02 -0400

    Implemented delete and rename card actions

--- components/actions.tsx
+++ components/actions.tsx
@@ -10,10 +10,11 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { ConfirmModal } from "@/components/confirm-modal";
 import { Button } from "@/components/ui/button";
-import { Link2, Trash2 } from "lucide-react";
+import { Link2, Pencil, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { api } from "@/convex/_generated/api";
+import { useRenameModal } from "@/store/use-rename-modal";
 
 interface ActionsProps {
     children: React.ReactNode;
@@ -30,6 +31,7 @@ export const Actions = ({
     id,
     title,
 }: ActionsProps ) => {
+    const { onOpen } = useRenameModal()
     const { mutate, pending } = useApiMutation(api.board.remove)
 
     const onCopyLink = () => {
@@ -61,6 +63,13 @@ export const Actions = ({
                     <Link2 className="h-4 w-4 mr-2" />
                     Copy board link
                 </DropdownMenuItem>
+                <DropdownMenuItem 
+                    onClick={() => onOpen(id, title)}
+                    className="p-3 cursor-pointer" 
+                >
+                    <Pencil className="h-4 w-4 mr-2" />
+                    Rename
+                </DropdownMenuItem>
                 <ConfirmModal
                     header="Delete board?"
                     description="This will delete the board and all of its contents."
@@ -71,7 +80,6 @@ export const Actions = ({
                         variant="ghost"
                         className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
                     >
-                        {/* 20:14 Card Actions */}
                         <Trash2 className="h-4 w-4 mr-2" />
                         Delete
                     </Button>
